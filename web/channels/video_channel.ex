684c344204ec9d09c1f5f9987294b3b223a92700
dashingshakeel <shkeelrauf@gmail.com>
2016-07-20 19:57:53 +0500

    video chanel j added and annotation added

---
+++ web/channels/video_channel.ex
@@ -0,0 +1,17 @@
+defmodule Rumbl.VideoChannel do
+  use Rumbl.Web, :channel
+  alias Rumbl.AnnotationView
+
+  def join("videos:" <> video_id, _params, socket) do
+  :timer.send_interval(5_000, :ping)
+  {:ok, socket}
+  end
+  def handle_in("new_annotation", params, socket) do
+  broadcast! socket, "new_annotation", %{
+  user: %{username: "anon"},
+  body: params["body"],
+  at: params["at"]
+  }
+  {:reply, :ok, socket}
+  end
+end

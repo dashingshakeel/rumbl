b41af438da01d1248416ec4e892e592f8b87c1ca
dashingshakeel <shkeelrauf@gmail.com>
2016-07-20 20:37:06 +0500

    annotation connected to database and stored in database

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -2,39 +2,39 @@ import Player from "./player"
 
 let Video = {
 
-init(socket, element){ if(!element){ return }
-let playerId = element.getAttribute("data-player-id")
-let videoId = element.getAttribute("data-id")
-socket.connect()
-Player.init(element.id, playerId, () => {
-this.onReady(videoId, socket)
-})
-},
-onReady(videoId, socket){
-let msgContainer = document.getElementById("msg-container")
-let msgInput
-= document.getElementById("msg-input")
-let postButton
-= document.getElementById("msg-submit")
-let vidChannel= socket.channel("videos:" + videoId)
-postButton.addEventListener("click", e => {
-let payload = {body: msgInput.value, at: Player.getCurrentTime()}
-vidChannel.push("new_annotation", payload)
-.receive("error", e => console.log(e) )
-msgInput.value = ""
-})
+    init(socket, element){ if(!element){ return }
+    let playerId = element.getAttribute("data-player-id")
+    let videoId = element.getAttribute("data-id")
+    socket.connect()
+    Player.init(element.id, playerId, () => {
+    this.onReady(videoId, socket)
+    })
+    },
+    onReady(videoId, socket){
+    let msgContainer = document.getElementById("msg-container")
+    let msgInput
+    = document.getElementById("msg-input")
+    let postButton
+    = document.getElementById("msg-submit")
+    let vidChannel= socket.channel("videos:" + videoId)
+    postButton.addEventListener("click", e => {
+    let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+    vidChannel.push("new_annotation", payload)
+    .receive("error", e => console.log(e) )
+    msgInput.value = ""
+    })
 
-vidChannel.on("new_annotation", (resp) => {
-this.renderAnnotation(msgContainer, resp)
-})
-// TODO join the vidChannel
+    vidChannel.on("new_annotation", (resp) => {
+    this.renderAnnotation(msgContainer, resp)
+    })
+    // TODO join the vidChannel
 
- vidChannel.join()
-.receive("ok", ({annotations}) => {
-annotations.forEach( ann => this.renderAnnotation(msgContainer, ann) )
-})
-.receive("error", reason => console.log("join failed", reason) )
-  },
+     vidChannel.join()
+    .receive("ok", ({annotations}) => {
+    annotations.forEach( ann => this.renderAnnotation(msgContainer, ann) )
+    })
+    .receive("error", reason => console.log("join failed", reason) )
+      },
 
   esc(str){ 
     let div = document.createElement("div")
